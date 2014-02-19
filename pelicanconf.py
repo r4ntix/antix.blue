bdb44df39e60b0587d644ac6884a27b5680fc757
r.4ntix Shawn <r.4ntix@gmail.com>
2014-02-19 21:47:51 +0800

    add Readability link for header bar.

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,7 +32,8 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Social
-SOCIAL = (('Instagram', 'http://instagram.com/r4ntix'),
+SOCIAL = (('Readability', 'https://www.readability.com/r4ntix/'),
+          ('Instagram', 'http://instagram.com/r4ntix'),
           ('Twitter', 'https://twitter.com/r4ntix'),
           ('Google+', 'https://plus.google.com/112109872395334308066'),)
 
